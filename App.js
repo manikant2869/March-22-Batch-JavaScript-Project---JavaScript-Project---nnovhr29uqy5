06b17d1f9fb02a1aa6c520e81a44ab75672dff10
Manikant Singh <manikant2869@gmail.com>
2022-11-01 23:00:32 +0530

    completed

--- App.js
+++ App.js
@@ -1,1 +1,25 @@
+(function(){
+    let buttons = document.querySelectorAll('.btn');
+    let screen = document.querySelector('.screen')
+    let clear = document.querySelector(".btn-clear")
+    let equal = document.querySelector('.btn-equal')
 
+   buttons.forEach((button)=>{
+       button.addEventListener('click',(event)=>{
+           let val = event.target.dataset.num;
+           screen.value+= val;
+       })
+   })
+   equal.addEventListener('click',(event)=>{
+       if(screen.value === ""){
+           screen.value = "";
+       }else{
+           let answer = eval(screen.value);
+           screen.value = answer;
+       }
+   })
+   clear.addEventListener('click',()=>{
+       screen.value = "";
+   })
+
+})();
